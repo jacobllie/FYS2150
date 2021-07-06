6941ab9c25096fd0c454b1ebe69d436f7ff35f57
jacob lie <jacob.l.lie97@gmail.com>
2021-07-06 12:05:13 +0200

    Korrekt versjon

--- program_conversion/FFTlyd.py
+++ program_conversion/FFTlyd.py
@@ -3,30 +3,32 @@ import numpy as np
 from sound_aqcuisition import sound_data_aqcuisition
 import soundfile as sf
 import matplotlib.pyplot as plt
-%matplotlib auto
+#%matplotlib auto
 #Ta opp lyd-data for å finne egenfrekvensen til messingsstaven
 #Jacob Lie 22.4.21, basert på matlab kode fra Alex Read 1.3.18
 
-duration = 5
-samplerate = 96000
-fmin = 0.8e3
+fmin = 500
 
-print("{} samples skal registreres".format(samplerate*duration))
 
-"""
-Hvis du allerede har data, kan du lese det inn i linjen under.
-Men da er det viktig at du vet samplerate og varighet på lydklippet.
-"""
+inputs = input("Har du en lydfil fra før? [Y/n] ")
 
-#mydata, samplerate = sf.read("purest_sound_ever.wav")
-#t = np.linspace(0,len(mydata)//samplerate,len(mydata))
-"""
-    Hvis du ikke har data, kan du kjøre linjen
+if inputs == "Y" or inputs == "y":
+    filename = input("Skriv inn filnavn på formen filnavn.wav ")
+    mydata, samplerate = sf.read(filename)
+    t = np.linspace(0,len(mydata)//samplerate,len(mydata))
+
+if inputs == "N" or inputs == "n":
+    #samplerate, duration = input("Skriv inn samplerate og duration på opptaket ")
+    temp = input("Skriv inn samplerate og duration ").split()
+    samplerate = int(temp[0])
+    duration = int(temp[1])
     mydata = sound_data_aqcuisition(duration, samplerate)
-"""
 
-t = np.linspace(0, duration, samplerate*duration)
-mydata = sound_data_aqcuisition(duration, samplerate)
+
+#duration = 5
+#samplerate = 96000
+#t = np.linspace(0, duration, samplerate*duration)
+#mydata = sound_data_aqcuisition(duration, samplerate)
 
 plt.subplot(121)
 plt.xlabel("Tid [s]")
@@ -38,21 +40,13 @@ n = len(mydata)
 Y = np.fft.fft(mydata)
 power = np.abs(Y[:len(Y)//2])**2
 
-
-
-
-FFT_freq = samplerate//40*np.linspace(0,1,len(power))
+FFT_freq = samplerate//2*np.linspace(0,1,len(power))  #Deler på to pga. Nyquist
 
 fmin_index = np.where(FFT_freq >= fmin)
-print(fmin_index)
-
 FFT_freq = FFT_freq[np.min(fmin_index):]
 power = power[np.min(fmin_index):]
 
-print(len(power))
-print(len(FFT_freq)) 
 
-#FFT_freq = samplerate*np.linspace(0,1,len(Y)//2)
 
 plt.subplot(122)
 plt.xlabel("Frekvens [Hz]")
@@ -60,6 +54,6 @@ plt.ylabel("Amplitude [a.u.]")
 plt.plot(FFT_freq,power)
 plt.show()
 
-#Finner egenfrekvensen til staven
+#Finner egenfrekvensen til staven.
 
 print("Grunntonen er {:.4} Hz".format(FFT_freq[np.argmax(power)]))
