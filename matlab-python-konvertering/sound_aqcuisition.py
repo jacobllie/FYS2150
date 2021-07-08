b80d04a1234ee8064d07015b5c415229dbd9cd9d
jacobllie <72126694+jacobllie@users.noreply.github.com>
2021-07-08 15:11:48 +0200

    lagt til kommentarer

--- matlab-python-konvertering/sound_aqcuisition.py
+++ matlab-python-konvertering/sound_aqcuisition.py
@@ -2,12 +2,32 @@ import sounddevice as sd
 import soundfile as sf
 
 def sound_data_aqcuisition(duration,samplerate):
-    sd.default.latency = 'low'
+    """
+    Bruker sounddevice for å ta opp lyd fra mikrofon, og soundfile
+    for å skrive dataen til fil. 
+    Parametere
+    ----------
+    duration : int
+        Varighet på opptak.
+    samplerate : int
+        Hvor mange samples som tas opp per sekund.
+
+    Returns
+    -------
+    mydata : array
+        data fra lydopptak.
+
+    """
+
+    sd.default.latency = 'low' #var dette som fungerte best
     print("Data aqcuisition started...")
-    mydata = sd.Stream(samplerate, blocksize = 1024, channels = 1)
+    #setter opp en stream med kontinuerlig input output
+    #dette kan tas opp av sd.rec for data behandling. 
+    mydata = sd.Stream(samplerate, blocksize = 1024, channels = 1) 
     mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
-                    channels=1, blocking=True)
-    sd.wait()
+                    channels=1, blocking=True) 
+    #blocking = True, gjør at outputen til sd.rec ikke kommer før opptaket er
+    #fullført.
 
     save = input("Vil du lagre dataene som en .wav fil [Y/n]?  ")
     if save == "Y" or save == "y":
